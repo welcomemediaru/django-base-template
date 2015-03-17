ecfb3f35caf797f94a241797ff96b00aed0c54c2
Vadim Yalo <yalovek@gmail.com>
2015-03-17 11:58:04 +0300

    flexboxtweener

--- base/static/js/main.js
+++ base/static/js/main.js
@@ -5,5 +5,7 @@
         $("body").width("100%").width("");
 
         $("[src$=svg]").svgmagic();
+
+        Modernizr.addTest("flexboxtweener", Modernizr.testAllProps("flexAlign", "end", true));
     });
 })(window.jQuery);
